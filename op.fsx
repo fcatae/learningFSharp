48b300cedecd7e9d2809f69e488dc383d724b2df
Fabricio Catae <fcatae@microsoft.com>
2017-02-28 20:48:03 -0300

    etc

---
+++ op.fsx
@@ -0,0 +1,26 @@
+
+
+
+
+// int -> int -> int
+let multiply a b = (a * b)
+
+// int -> int
+let duplicar = multiply 2
+
+multiply 2 4
+duplicar 4
+
+// multiply: int -> int -> int
+// duplicar: (int -> int )
+
+
+
+(3, 2) ||> multiply
+
+"123456".[3..4]
+
+
+    let multiply2 (a,b) = (a*b)
+
+multiply 5 , 5 
